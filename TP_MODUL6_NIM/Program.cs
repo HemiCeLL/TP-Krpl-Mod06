c6eced4dc789644b96b1ab314f6b70d6a3c9f025
HemiCeLL <maxthejanitor360@gmail.com>
2026-04-06 04:07:59 +0700

    commit message

--- TP_MODUL6_NIM/Program.cs
+++ TP_MODUL6_NIM/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MusicApp
+{
+    public class SayaMusicTrack
+    {
+        private int id;
+        private string playCount;
+        private string title;
+
+        public SayaMusicTrack(string title)
+        {
+            this.title = title;
+
+            Random rand = new Random();
+            this.id = rand.Next(10000, 100000);
+
+            this.playCount = "0";
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            int currentCount = int.Parse(this.playCount);
+            int newCount = currentCount + count;
+            this.playCount = newCount.ToString();
+            Console.WriteLine($"Berhasil menambah {count} play.");
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("\n============================");
+            Console.WriteLine($"TRACK ID    : {this.id}");
+            Console.WriteLine($"TITLE       : {this.title}");
+            Console.WriteLine($"PLAY COUNT  : {this.playCount}");
+            Console.WriteLine("============================");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Menginisialisasi Lagu Baru ---");
+            SayaMusicTrack lagu = new SayaMusicTrack("Chop Suey!");
+
+            lagu.PrintTrackDetails();
+
+            Console.WriteLine("\n--- Menambahkan Play Count ---");
+            lagu.IncreasePlayCount(14;
+            lagu.IncreasePlayCount(69);
+
+            lagu.PrintTrackDetails();
+
+            Console.WriteLine("\nTekan tombol apa saja untuk keluar...");
+            Console.ReadKey();
+        }
+    }
+}
--- TP_MODUL6_NIM/obj/Debug/net10.0/TP_MODUL6_NIM.AssemblyInfo.cs
+++ TP_MODUL6_NIM/obj/Debug/net10.0/TP_MODUL6_NIM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_NIM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe21c8014e74be30656e89f73367d832baec58c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_NIM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_NIM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
